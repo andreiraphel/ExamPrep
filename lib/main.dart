cb09c3643d6e81c9e4cafa18735b9a55ec900b59
andreiraphel <andreiraphel@gmail.com>
2024-06-01 12:03:51 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,12 @@
 import 'package:flutter/material.dart';
 
-class CounterDisplay extends StatelessWidget {
-  const CounterDisplay({required this.count, super.key});
-
-  final int count;
-
-  @override
-  Widget build(BuildContext context) {
-    return Text('Count: $count');
-  }
-}
-
-class CounterIncrementor extends StatelessWidget {
-  const CounterIncrementor({required this.onTap, super.key});
-
-  final VoidCallback onTap;
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      onTap: onTap,
-      title: const Text('Test1'),
-    );
-  }
-}
-
-class Counter extends StatefulWidget {
-  const Counter({super.key});
-
-  @override
-  State<Counter> createState() => _CounterState();
-}
-
-class _CounterState extends State<Counter> {
-  int _counter = 0;
-
-  void _increment() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _decrement() {
-    setState(() {
-      _counter--;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        CounterDisplay(count: _counter),
-        CounterIncrementor(onTap: _increment)
-      ],
-    );
-  }
+void main() {
+  runApp(
+    const MaterialApp(
+      title: 'ExamPrep',
+      home: Home(),
+    ),
+  );
 }
 
 class Home extends StatelessWidget {
@@ -69,18 +19,49 @@ class Home extends StatelessWidget {
         title: const Text('ExamPrep'),
         backgroundColor: Colors.blueGrey,
       ),
-      body: const Center(
-        child: Counter(),
-      ),
+      body: Checklist(),
+      floatingActionButton: AddItem(),
     );
   }
 }
 
-void main() {
-  runApp(
-    const MaterialApp(
-      title: 'ExamPrep',
-      home: Home(),
-    ),
-  );
+class Checklist extends StatefulWidget {
+  const Checklist({super.key});
+
+  @override
+  State<Checklist> createState() => _ChecklistState();
+}
+
+class _ChecklistState extends State<Checklist> {
+  List<String> items = [];
+
+  @override
+  Widget build(BuildContext context) {
+    if (items.isEmpty) {
+      return const Center(
+        child: Text('Please add a topic'),
+      );
+    } else {
+      return ListView.builder(
+          itemCount: items.length,
+          itemBuilder: ((context, index) {
+            return ListTile(
+              title: Text(items[index]),
+            );
+          }));
+    }
+  }
+}
+
+class AddItem extends StatelessWidget {
+  const AddItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () {},
+      tooltip: 'Add Item',
+      child: Icon(Icons.add),
+    );
+  }
 }
