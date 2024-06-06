b4e452abf58afe92fa20de7e0bad3c6462400caa
ymirchang <157774788+ymirchang@users.noreply.github.com>
2024-06-06 21:04:36 +0800

    Update topics.dart
    akong gi update haha

--- lib/widgets/topics.dart
+++ lib/widgets/topics.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import '../database_helper.dart';
 import '../screens/new_card.dart';
 import '../screens/topic_details.dart';
+import '../screens/add_topic_screen.dart';
+import '../screens/settings_screen.dart'; 
 
 class Topics extends StatefulWidget {
   const Topics({Key? key}) : super(key: key);
@@ -22,15 +24,14 @@ class TopicsState extends State<Topics> {
   }
 
   void _loadTopics() async {
-    List<Map<String, dynamic>> topics = await dbHelper.getTopics();
-    setState(() {
-      items = topics;
-    });
-  }
-
-  void addItem(String newItem) async {
-    await dbHelper.insertTopic(newItem);
-    _loadTopics();
+    try {
+      List<Map<String, dynamic>> topics = await dbHelper.getTopics();
+      setState(() {
+        items = topics;
+      });
+    } catch (e) {
+      print('Error loading topics: $e');
+    }
   }
 
   void toggleDelete() {
@@ -65,71 +66,116 @@ class TopicsState extends State<Topics> {
     );
 
     if (confirmDelete == true) {
-      await dbHelper.deleteTopic(id);
-      _loadTopics();
+      try {
+        await dbHelper.deleteTopic(id);
+        _loadTopics();
+      } catch (e) {
+        print('Error deleting item: $e');
+      }
     }
+  }
+
+  Future<void> _navigateToAddTopicScreen() async {
+    bool? topicAdded = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const AddTopicScreen()),
+    );
+    if (topicAdded == true) {
+      _loadTopics(); 
+    }
+  }
+
+  Future<void> _navigateToSettingsScreen() async {
+    await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const SettingsScreen()),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return items.isEmpty
-        ? const Center(
-            child: Text('Add a topic'),
-          )
-        : ListView.builder(
-            itemCount: items.length,
-            itemBuilder: ((context, index) {
-              return ListTile(
-                title: Text(items[index]['name']),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => TopicDetails(
-                              topicId: items[index]['id'],
-                              topicName: items[index]['name'],
-                            )),
-                  );
-                },
-                trailing: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    if (deleteBool)
-                      IconButton(
-                        icon: Icon(Icons.delete),
-                        color: Colors.red,
-                        onPressed: () {
-                          deleteItem(items[index]['id']);
-                        },
-                      )
-                    else ...[
-                      IconButton(
-                        onPressed: () {
-                          // ADD FLASHCARD FUNCTION
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => NewCard(
-                                        topicName: items[index]['name'],
-                                        topicId: items[index]['id'],
-                                      )));
-                        },
-                        icon: Icon(Icons.add),
-                        color: Color(0xFF76ABAE),
-                      ),
-                      const SizedBox(width: 10),
-                      IconButton(
-                        onPressed: () {
-                          // IMPLEMENT START FUNCTION
-                        },
-                        icon: Icon(Icons.play_arrow),
-                        color: Color(0xFF76ABAE),
-                      ),
-                    ]
-                  ],
-                ),
-              );
-            }),
-          );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Topics'),
+        actions: [
+          IconButton(
+            icon: Icon(deleteBool ? Icons.cancel : Icons.delete),
+            onPressed: toggleDelete,
+          ),
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: _navigateToSettingsScreen,
+          ),
+        ],
+      ),
+      body: items.isEmpty
+          ? const Center(
+              child: Text('Add a topic'),
+            )
+          : ListView.builder(
+              itemCount: items.length,
+              itemBuilder: ((context, index) {
+                return _buildTopicItem(context, index);
+              }),
+            ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _navigateToAddTopicScreen,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  Widget _buildTopicItem(BuildContext context, int index) {
+    return ListTile(
+      title: Text(items[index]['name']),
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => TopicDetails(
+              topicId: items[index]['id'],
+              topicName: items[index]['name'],
+            ),
+          ),
+        );
+      },
+      trailing: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          if (deleteBool)
+            IconButton(
+              icon: Icon(Icons.delete),
+              color: Colors.red,
+              onPressed: () {
+                deleteItem(items[index]['id']);
+              },
+            )
+          else ...[
+            IconButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => NewCard(
+                      topicName: items[index]['name'],
+                      topicId: items[index]['id'],
+                    ),
+                  ),
+                );
+              },
+              icon: Icon(Icons.add),
+              color: Color(0xFF76ABAE),
+            ),
+            const SizedBox(width: 10),
+            IconButton(
+              onPressed: () {
+              },
+              icon: Icon(Icons.play_arrow),
+              color: Color(0xFF76ABAE),
+            ),
+          ]
+        ],
+      ),
+    );
   }
 }
